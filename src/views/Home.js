94344b3f857bba3b9ad70b48fdcbcc955f2518ac
josephrockqz <josephrockqz@gmail.com>
2022-06-21 09:57:01 -0500

    fixed styling of series stats component

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,13 +4,13 @@ import LevelSelect from '../components/LevelSelect.js';
 import NavBar from '../components/NavBar.js';
 
 import { DiGithubBadge } from "react-icons/di";
-import { BiCopyright, BiPointer } from "react-icons/bi";
+import { BiCopyright } from "react-icons/bi";
 
 export default function Home() {
     const linkGithub = () => {
         console.log("hello");
         window.open('https://github.com/josephrockqz/landmarker', '_blank');
-    }
+    };
 
     return (
         <div>
@@ -19,7 +19,7 @@ export default function Home() {
             <div className="top-of-page">
                 <p className="home-text">
                     Click on the globe where you think each prompted landmark is located. 
-                    The closer you are, the better your score.
+                    The closer you are, the better your score. Choose one of the 5 levels.
                 </p>
             </div>
 
@@ -32,10 +32,23 @@ export default function Home() {
                 </p>
             </div>
 
+            
+
+            <div className="top-of-page">
+                <p className="home-text">
+                    Note: personal stats will not be saved if local storage is disabled.
+                </p>
+            </div>
+
             <div className="footer-div">
                 <span className="footer-span">
                     Creator: Joseph Rock
-                    <DiGithubBadge color="black" size="20px" style={{cursor: 'pointer'}} onClick={() => linkGithub()} />
+                    <DiGithubBadge
+                        color="black" 
+                        size="20px" 
+                        style={{cursor: 'pointer'}} 
+                        onClick={() => linkGithub()}
+                    />
                 </span>
                 <span className="footer-span">
                     <BiCopyright />
