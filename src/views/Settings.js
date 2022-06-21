469284a82f44bf5ebce480e85a295cb690b9b3d3
josephrockqz <josephrockqz@gmail.com>
2022-06-21 12:03:30 -0500

    configured beginning of settings view

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -6,8 +6,45 @@ export default function Settings() {
     return (
         <div>
             <NavBar />
+
             <div className="top-of-page">
-                
+
+                <div className="settings-row">
+                    <span>
+                        Language
+                    </span>
+                    <span>
+                        hello
+                    </span>
+                </div>
+
+                <div className="settings-row">
+                    <span>
+                        Borders
+                    </span>
+                    <span>
+                        hello
+                    </span>
+                </div>
+
+                <div className="settings-row">
+                    <span>
+                        Theme
+                    </span>
+                    <span>
+                        hello
+                    </span>
+                </div>
+
+                <div className="settings-row">
+                    <span>
+                        Metric System
+                    </span>
+                    <span>
+                        hello
+                    </span>
+                </div>
+
             </div>
         </div>
     );
