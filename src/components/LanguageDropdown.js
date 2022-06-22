8356134645a37ee765ecec9fdfe16643adb53f65
josephrockqz <josephrockqz@gmail.com>
2022-06-22 13:14:37 -0500

    componentized language dropdown

---
+++ src/components/LanguageDropdown.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { UserContext } from "../index.js";
+
+export default function LanguageDropdown() {
+    const [ state, dispatch ] = useContext(UserContext);
+
+    const handleClick = num =>  {
+        switch(num) {
+            case 1:
+                dispatch({ type: "update_language", payload: "English"});
+                return;
+            case 2:
+                dispatch({ type: "update_language", payload: "Spanish"});
+                return;
+            case 3:
+                dispatch({ type: "update_language", payload: "Italian"});
+                return;
+            case 4:
+                dispatch({ type: "update_language", payload: "Russian"});
+                return;
+            default:
+                return;
+        }
+    }
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle>{state.language}</Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item onClick={() => handleClick(1)}>English</Dropdown.Item>
+                <Dropdown.Item onClick={() => handleClick(2)}>Spanish</Dropdown.Item>
+                <Dropdown.Item onClick={() => handleClick(3)}>Italian</Dropdown.Item>
+                <Dropdown.Item onClick={() => handleClick(4)}>Russian</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+}
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,30 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import LanguageDropdown from '../components/LanguageDropdown.js';
 import NavBar from '../components/NavBar.js';
-import { Dropdown } from 'react-bootstrap';
-import { UserContext } from "../index.js";
 
 export default function Settings() {
-    const [ state, dispatch ] = useContext(UserContext);
-
-    const handleClick = num =>  {
-        switch(num) {
-            case 1:
-                dispatch({ type: "update_language", payload: "English"});
-                return;
-            case 2:
-                dispatch({ type: "update_language", payload: "Spanish"});
-                return;
-            case 3:
-                dispatch({ type: "update_language", payload: "Italian"});
-                return;
-            case 4:
-                dispatch({ type: "update_language", payload: "Russian"});
-                return;
-            default:
-                return;
-        }
-    }
-
     return (
         <div>
             <NavBar />
@@ -36,16 +14,7 @@ export default function Settings() {
                         Language
                     </span>
                     <span>
-                        <Dropdown>
-                            <Dropdown.Toggle>{state.language}</Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                <Dropdown.Item onClick={() => handleClick(1)}>English</Dropdown.Item>
-                                <Dropdown.Item onClick={() => handleClick(2)}>Spanish</Dropdown.Item>
-                                <Dropdown.Item onClick={() => handleClick(3)}>Italian</Dropdown.Item>
-                                <Dropdown.Item onClick={() => handleClick(4)}>Russian</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
+                        <LanguageDropdown />
                     </span>
                 </div>
 
