1d0d837cdd074f4b469ff9fdbfe2a4b5052c1513
josephrockqz <josephrockqz@gmail.com>
2022-06-28 17:32:03 -0500

    fixed cookies errors by changing picture of globe start

--- src/components/GlobeStart.js
+++ src/components/GlobeStart.js
@@ -7,7 +7,7 @@ export default function GlobeStart() {
     return (
         <div style={{textAlign: 'center'}}>
             <img
-                src="https://freesvg.org/img/earth-globe-dan-gerhrad-05r.png"
+                src="https://cdn.pixabay.com/photo/2013/07/12/19/03/earth-154279_1280.png"
                 alt="Globe"
                 height="200"
                 width="200"
